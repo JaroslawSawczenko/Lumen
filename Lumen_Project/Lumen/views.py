2b6700a027b79fe2c88a6a48fc503834443ae630
Jarek <jareksavchenko@gmail.com>
2025-09-21 11:17:58 +0200

    Add changes to URLs and views

--- Lumen_Project/Lumen/views.py
+++ Lumen_Project/Lumen/views.py
@@ -3,12 +3,8 @@ from .models import Question
 
 
 def question_list(request):
-    """
-    Widok, który pobiera wszystkie pytania z bazy danych
-    i przekazuje je do szablonu.
-    """
     questions = Question.objects.all()
     context = {
         'questions': questions
     }
-    return render(request, 'Lumen/question_list.html', context)
+    return render(request, 'Lumen/Main.html', context)

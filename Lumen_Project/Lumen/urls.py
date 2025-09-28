e24e0ef7e607175cb6b7ef3c853e73fb64257415
Jarek <jareksavchenko@gmail.com>
2025-09-28 22:41:27 +0200

    calkowite dzalanie gry

--- Lumen_Project/Lumen/urls.py
+++ Lumen_Project/Lumen/urls.py
@@ -4,5 +4,5 @@ from .views import quiz_list, quiz_detail, play_quiz_view
 urlpatterns = [
     path('', quiz_list, name='quiz_list'),
     path('quiz/<int:quiz_id>/', quiz_detail, name='quiz_detail'),
-    path("quiz/<int:quiz_id>/pytanie/<int:question_order>", play_quiz_view, name="play_quiz_view" ),
+    path("quiz/<int:quiz_id>/pytanie/<int:question_order>/", play_quiz_view, name="play_quiz_view" ),
 ]
